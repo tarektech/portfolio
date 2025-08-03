9eb7972c2ce0459a15252434b339851744906cbf
tarek alzein <tarekalzein97@gmail.com>
2025-08-03 13:17:43 +0300

    Update tech stack icons to include width and height attributes for improved rendering 0.1.1

--- src/lib/techStackIcons.tsx
+++ src/lib/techStackIcons.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import Image from 'next/image';
 export const HtmlIcon = () => (
-  <Image src="/techstack/html5.svg" alt="HTML" className="w-14 h-14 " />
+  <Image
+    src="/techstack/html5.svg"
+    alt="HTML"
+    className="w-14 h-14 "
+    width={56}
+    height={56}
+  />
 );
 export const CssIcon = () => (
-  <Image src="/techstack/css3.svg" alt="CSS" className="w-14 h-14" />
+  <Image
+    src="/techstack/css3.svg"
+    alt="CSS"
+    className="w-14 h-14"
+    width={56}
+    height={56}
+  />
 );
 export const JsIcon = () => (
   <Image
     src="/techstack/javascript.svg"
     alt="JavaScript"
     className="w-14 h-14"
+    width={56}
+    height={56}
   />
 );
 export const TailwindIcon = () => (
@@ -18,28 +32,56 @@ export const TailwindIcon = () => (
     src="/techstack/tailwindcss.svg"
     alt="Tailwind"
     className="w-14 h-14"
+    width={56}
+    height={56}
   />
 );
 export const NodeIcon = () => (
-  <Image src="/techstack/nodejs.svg" alt="Node.js" className="w-14 h-14" />
+  <Image
+    src="/techstack/nodejs.svg"
+    alt="Node.js"
+    className="w-14 h-14"
+    width={56}
+    height={56}
+  />
 );
 export const MongoIcon = () => (
-  <Image src="/techstack/mongodb.svg" alt="MongoDB" className="w-14 h-14" />
+  <Image
+    src="/techstack/mongodb.svg"
+    alt="MongoDB"
+    className="w-14 h-14"
+    width={56}
+    height={56}
+  />
 );
 
 export const ReactIcon = () => (
-  <Image src="/techstack/react.svg" alt="React" className="w-14 h-14" />
+  <Image
+    src="/techstack/react.svg"
+    alt="React"
+    className="w-14 h-14"
+    width={56}
+    height={56}
+  />
 );
 export const TypescriptIcon = () => (
   <Image
     src="/techstack/typescript.svg"
     alt="Typescript"
     className="w-14 h-14"
+    width={56}
+    height={56}
   />
 );
 
 export const DockerIcon = () => (
-  <Image src="/techstack/docker.svg" alt="Docker" className="w-14 h-14" />
+  <Image
+    src="/techstack/docker.svg"
+    alt="Docker"
+    className="w-14 h-14"
+    width={56}
+    height={56}
+  />
 );
 
 export const PostgreSQLIcon = () => (
@@ -47,13 +89,27 @@ export const PostgreSQLIcon = () => (
     src="/techstack/postgresql.svg"
     alt="PostgreSQL"
     className="w-14 h-14"
+    width={56}
+    height={56}
   />
 );
 
 export const ExpressIcon = () => (
-  <Image src="/techstack/ex.png" alt="Express.js" className="w-14 h-14 " />
+  <Image
+    src="/techstack/ex.png"
+    alt="Express.js"
+    className="w-14 h-14 "
+    width={56}
+    height={56}
+  />
 );
 
 export const JwtIcon = () => (
-  <Image src="/techstack/jwt.png" alt="JWT" className="w-14 h-14 " />
+  <Image
+    src="/techstack/jwt.png"
+    alt="JWT"
+    className="w-14 h-14 "
+    width={56}
+    height={56}
+  />
 );
